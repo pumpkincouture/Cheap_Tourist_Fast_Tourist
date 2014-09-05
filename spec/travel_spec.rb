78302f9966b8b7fd0d6e1e4cf1379db993bf127d
pumpkincouture <pumpkincouture@gmail.com>
2014-09-05 09:42:55 -0500

    Changes 9-5

--- spec/travel_spec.rb
+++ spec/travel_spec.rb
@@ -3,15 +3,15 @@ require_relative '../lib/flights.rb'
 require_relative 'spec_helper.rb'
 
 describe "TravelFinder" do
+	list = './lib/sample-input.csv'
 	
 	before :each do 
 		@travel_finder = TravelFinder.new
 	end
 
 	it "returns a flight list" do
-		# @travel_finder.get_flights('./lib/sample-input.csv')
-		expect(@travel_finder.get_flights('./lib/sample-input.csv')[3].origin).to eq("A")
-		expect(@travel_finder.get_flights('./lib/sample-input.csv')[3].destination).to eq("B")
-		expect(@travel_finder.get_flights('./lib/sample-input.csv')[4].departure).to eq("11:30")
+		expect(@travel_finder.get_flights(list)[3].origin).to eq("A")
+		expect(@travel_finder.get_flights(list)[3].destination).to eq("B")
+		expect(@travel_finder.get_flights(list)[4].departure).to eq("11:30")
 	end
 end
